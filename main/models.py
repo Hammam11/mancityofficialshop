6936caca29e8ccd4ec302329797f6249ccd3d47a
Hammam11 <hammam.muhammad@ui.ac.id>
2025-09-09 16:26:06 +0700

    Complete Tugas Individu 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('shoe', 'Shoe'),
+        ('ball', 'Ball'),
+        ('sock', 'Sock'),
+        ('accessories', 'Accessories'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField(default="")
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='jersey')
+    products_solds = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def is_popular(self):
+        return self.products_solds > 25
+        
+    def increment_solds(self):
+        self.products_solds += 1
+        self.save()
